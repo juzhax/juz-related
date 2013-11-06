3541de113342cc2e1b92d9b4fb19247aa6364d07
juzhax <juzhax@gmail.com>
2013-11-07 03:43:46 +0700

    Fixed all bugs, almost done.

--- juz_related.php
+++ juz_related.php
@@ -7,6 +7,22 @@ Version: 1.0
 Author: Justin Soo ( JuzHax )
 Author URI: http://www.juzhax.com/
 License: GPL2
+
+CREATE TABLE IF NOT EXISTS `wp_juz_related_id` (
+  `post_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  UNIQUE KEY `post_id` (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `wp_juz_related_item` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_title` varchar(255) NOT NULL,
+  `item_data` text NOT NULL,
+  `item_update` datetime NOT NULL,
+  UNIQUE KEY `item_id` (`item_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
 */
 
 add_action( 'admin_menu', 'wp_juz_related_menu' );
@@ -161,13 +177,7 @@ function wp_juz_related_form($action='add') {
 				array (
 					'%d',
 				) 
-				
 			);  		
-			
-			
-			
-			
-			
 		}
 		foreach ($post_ids as $post_id) {
 			$post_id = trim($post_id);
@@ -321,10 +331,8 @@ function wp_juz_related_get_post_ids($item_id) {
 	}
 }
 
-
 function wp_juz_related_list() {
 	global $wpdb;
-	
 
 	$JuzRelatedTable = new Juz_Related_List_Table();	
 
@@ -334,94 +342,11 @@ function wp_juz_related_list() {
 	$JuzRelatedTable->prepare_items(); 
 	$JuzRelatedTable->display(); 
 	echo '</div>'; 
-
 }
 
-
-
-
-function wp_jauz_related() {
-//	global $wp_query;
+function wp_juz_related() {
 	global $wpdb;
-	
 	$content = '';
-	$wp_juz = array();
-	$wp_juz_post_id = array (9,280,210,229,249,266,1999);
-	
-	foreach ($wp_juz_post_id as $post_id) {
-		$wp_juz_group[$post_id] = 'icomania';
-	}	
-	
-	
-	$wp_juz_data['icomania'] = '
-	<h3>Other Language for Icomania Answers:</h3>
-	<ul>
-	<li><a title="Icomania Answers" href="http://game-solver.com/icomania-answers/">Icomania Answers</a></li>
-	<li><a title="Icomania Antwoorden Nederlands" href="http://game-solver.com/icomania-antwoorden-nederlands/">Icomania Antwoorden Nederlands</a></li>
-	<li><a title="Icomania astuce soluce Français" href="http://game-solver.com/icomania-astuce-soluce-francais/">Icomania astuce soluce Français</a></li>
-	<li><a title="Icomania Lösung Deutsch" href="http://game-solver.com/icomania-deutsch-losung/">Icomania Lösung Deutsch</a></li>
-	<li><a title="Icomania Respuestas Soluciones Espanol" href="http://game-solver.com/icomania-respuestas-soluciones-espanol/">Icomania Respuestas Soluciones Espanol</a></li>
-	<li><a title="Icomania Soluzioni Italiano" href="http://game-solver.com/icomania-soluzioni-italiano/">Icomania Soluzioni Italiano</a></li>
-	<li><a title="Icomania ответы" href="http://game-solver.com/icomania-%d0%be%d1%82%d0%b2%d0%b5%d1%82%d1%8b/">Icomania ответы</a></li>
-	</ul>';
-
-	$wp_juz_post_id = array (6709,3762,2916,3414,6686,6622,8069,6637,3014,3455,3086,8032,6766,6651,3776,8055);
-	foreach ($wp_juz_post_id as $post_id) {
-		$wp_juz_group[$post_id] = 'higuess';
-	}	
-
-	
-	$wp_juz_data['higuess'] = '
-	<h3>Other Hi Guess Games Answers:</h3>
-<ul>
-	<li><a title="Hi Guess the Basketball Answers" href="http://game-solver.com/hi-guess-the-basketball-answers/">Hi Guess the Basketball Answers</a></li>
-	<li><a title="Hi Guess the Brand Answers" href="http://game-solver.com/hi-guess-the-brand-answers/">Hi Guess the Brand Answers</a></li>
-	<li><a title="Hi Guess the Character Answers" href="http://game-solver.com/hi-guess-the-character-answers/">Hi Guess the Character Answers</a></li>
-	<li><a title="Hi Guess the Emoji Answers" href="http://game-solver.com/hi-guess-the-emoji-answers/">Hi Guess the Emoji Answers</a></li>
-	<li><a title="Hi Guess the Food Answers" href="http://game-solver.com/hi-guess-the-food-answers/">Hi Guess the Food Answers</a></li>
-	<li><a title="Hi Guess the Football Star Answers" href="http://game-solver.com/hi-guess-the-football-star-answers/">Hi Guess the Football Star Answers</a></li>
-	<li><a title="Hi Guess the Games Answers" href="http://game-solver.com/hi-guess-the-games-answers/">Hi Guess the Games Answers</a></li>
-	<li><a title="Hi Guess the Movie Answers" href="http://game-solver.com/hi-guess-the-movie-answers/">Hi Guess the Movie Answers</a></li>
-	<li><a title="Hi Guess the Pic Answers" href="http://game-solver.com/hi-guess-the-pic-answers/">Hi Guess the Pic Answers</a></li>
-	<li><a title="Hi Guess the Place Answers" href="http://game-solver.com/hi-guess-the-place-answers/">Hi Guess the Place Answers</a></li>
-	<li><a title="Hi Guess the Restaurant Answers" href="http://game-solver.com/hi-guess-the-restaurant-answers/">Hi Guess the Restaurant Answers</a></li>
-	<li><a title="Hi Guess the Riddle Answers" href="http://game-solver.com/hi-guess-the-riddle-answers/">Hi Guess the Riddle Answers</a></li>
-	<li><a title="Hi Guess the Show Answers" href="http://game-solver.com/hi-guess-the-show-answers/">Hi Guess the Show Answers</a></li>
-	<li><a title="Hi Guess the TV Show Answers" href="http://game-solver.com/hi-guess-the-tv-show-answers/">Hi Guess the TV Show Answers</a></li>
-	<li><a title="Hi Guess the View Answers" href="http://game-solver.com/hi-guess-the-view-answers/">Hi Guess the View Answers</a></li>
-	<li><a title="Hi Guess Who Answers" href="http://game-solver.com/hi-guess-who-answers/">Hi Guess Who Answers</a></li>
-</ul>
-';	
-
-	$wp_juz_post_id = array (7722,7725,7728,7731,7734,7737,7741,7744,7747,7750,7753,7756,7759,7762,7765,7768);
-	foreach ($wp_juz_post_id as $post_id) {
-		$wp_juz_group[$post_id] = 'flowfree';
-	}	
-
-	$wp_juz_data['flowfree'] = '
-	<h3>All others Flow Free Solutions:</h3>
-	
-<ul>
-	<li><a title="Flow Regular Pack Solutions" href="http://game-solver.com/flow-regular-pack-solutions/">Flow Regular Pack Solutions</a></li>
-	<li><a title="Flow Bonus Pack Solutions" href="http://game-solver.com/flow-bonus-pack-solutions/">Flow Bonus Pack Solutions</a></li>
-	<li><a title="Flow Green Pack Solutions" href="http://game-solver.com/flow-green-pack-solutions/">Flow Green Pack Solutions</a></li>
-	<li><a title="Flow Blue Pack Solutions" href="http://game-solver.com/flow-blue-pack-solutions/">Flow Blue Pack Solutions</a></li>
-	<li><a title="Flow Bridge Pack Solutions" href="http://game-solver.com/flow-bridge-pack-solutions/">Flow Bridge Pack Solutions</a></li>
-	<li><a title="Flow 7×7 Mania Solutions" href="http://game-solver.com/flow-7x7-mania-solutions/">Flow 7×7 Mania Solutions</a></li>
-	<li><a title="Flow 8×8 Mania Solutions" href="http://game-solver.com/flow-8x8-mania-solutions/">Flow 8×8 Mania Solutions</a></li>
-	<li><a title="Flow 9×9 Mania Solutions" href="http://game-solver.com/flow-9x9-mania-solutions/">Flow 9×9 Mania Solutions</a></li>
-	<li><a title="Flow 10×10 Mania Solutions" href="http://game-solver.com/flow-10x10-mania-solutions/">Flow 10×10 Mania Solutions</a></li>
-	<li><a title="Flow 11×11 Mania Solutions" href="http://game-solver.com/flow-11x11-mania-solutions/">Flow 11×11 Mania Solutions</a></li>
-	<li><a title="Flow 12×12 Mania Solutions" href="http://game-solver.com/flow-12x12-mania-solutions/">Flow 12×12 Mania Solutions</a></li>
-	<li><a title="Flow Rainbow Pack Solutions" href="http://game-solver.com/flow-rainbow-pack-solutions/">Flow Rainbow Pack Solutions</a></li>
-	<li><a title="Flow Kids Pack Solutions" href="http://game-solver.com/flow-kids-pack-solutions/">Flow Kids Pack Solutions</a></li>
-	<li><a title="Flow Jumbo Pack Solutions" href="http://game-solver.com/flow-jumbo-pack-solutions/">Flow Jumbo Pack (iPad) Solutions</a></li>
-	<li><a title="Flow Purple Pack Solutions" href="http://game-solver.com/flow-purple-pack-solutions/">Flow Purple Pack (iPad) Solutions</a></li>
-	<li><a title="Flow Pink Pack Solutions" href="http://game-solver.com/flow-pink-pack-solutions/">Flow Pink Pack (iPad) Solutions</a></li>
-</ul>
-';	
-
-	
 	if (is_single()) {
 		$get_the_ID = get_the_ID();
 		$wp_juz_data = $wpdb->get_row('SELECT post_id, `wp_juz_related_id`.item_id, item_data
@@ -429,32 +354,11 @@ function wp_jauz_related() {
 					LEFT JOIN `wp_juz_related_item`
 					ON `wp_juz_related_id`.item_id =`wp_juz_related_item`.item_id
 					WHERE post_id = '.$get_the_ID.';',ARRAY_A);
-		echo '<pre>';
-		print_r($wp_juz_data);
-		echo '</pre>';
 
-		if (isset($wp_juz_group[$get_the_ID])) {
-			$content .= $wp_juz_data[$wp_juz_group[$get_the_ID]];
-//			$content = $get_the_ID;
-//			print_r($wp_juz[$get_the_ID]);
-/*
-			$content = '<h3>Other Related :</h3>';
-			$content .= '<ul>';
-			foreach ($wp_juz[$wp_juz_group[$get_the_ID]]['data'] as $data) {
-				$content .= '<li><a href="'.$data['url'].'" title="'. $data['title'].'">'. $data['title'].'</a></li>';
-			}
-			$content .= '</ul>';
-*/			
+		if (isset($wp_juz_data['item_data'])) {
+			$content = $wp_juz_data['item_data'];
 		}
-	
 	} 
 	return $content;
 }
-
-
-echo get_the_ID();
-// wp_juz_related();
-
-
-
 ?>
